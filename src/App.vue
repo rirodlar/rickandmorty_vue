7e5c4d8e303250e65687f1ac73ff15d57e42d892
ricardo <rarodriuezl@sodimac.cl>
2020-12-13 16:46:03 -0300

    creacion proyecto

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,46 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <div id="app" class="container">
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Rick and Morty</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <form class="d-flex">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <router-link class="nav-item nav-link"
+                               active-class="active"
+                               exact to="/">Home</router-link>
+                </li>
+
+                <li class="nav-item">
+                  <router-link     class="nav-item nav-link"
+                                   active-class="active"
+                                   exact  to="/opinion">Opiniones</router-link>
+                </li>
+
+                <li class="nav-item">
+                  <router-link    class="nav-item nav-link"
+                                  active-class="active"
+                                  exact to="/favorito">Favoritos</router-link>
+                </li>
+
+                <li class="nav-item">
+                  <router-link     class="nav-item nav-link"
+                                   active-class="active"
+                                   exact  to="/administracion">Administración</router-link>
+                </li>
+
+              </ul>
+
+            </div>
+        </form>
+      </div>
+    </nav>
+
     <router-view/>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="row">
+
+    <div v-for="(item) of personajes" :key="item.id" class="card"  style="width: 18rem;">
+      <div class="card-block d-flex flex-column">
+          <img class="card-img-top" :src="item.image">
+          <div class="card-body">
+            <h5 class="card-title">{{ item.name }}</h5>
+              <div class="d-flex justify-content-between">
+                <a href="#" class="btn btn-primary">Opinar</a>
+                <a href="#" class="btn btn-primary">ver</a>
+            </div>
+          </div>
+      </div>
+    </div>
+
+
   </div>
+
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      personajes: []
+    }
+   },
+  created() {
+    this.getPersonajes()
+  },
+  methods:{
+      async getPersonajes(){
+        try {
+          const personajes = await this.axios.get("https://rickandmortyapi.com/api/character");
+          this.personajes = personajes.data.results;
+
+
+        }catch (error){
+            console.log(error);
+        }
+      }
   }
 }
 </script>
